181e3e1964b85303b9ecb1892e717d574c997874
Iiro Alhonen <iiro.alhonen@gmail.com>
2017-07-27 09:30:07 +0300

    More work on the first level

--- From Dark/Assets/Assets/Scripts/PlayerObject.cs
+++ From Dark/Assets/Assets/Scripts/PlayerObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerObject : MonoBehaviour {
-
+	public Rigidbody2D rb;
 	private Vector3 spawn;
 	// Use this for initialization
 	void Start () {
