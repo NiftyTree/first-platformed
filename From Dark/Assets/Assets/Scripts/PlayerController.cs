08313056a77d3bb845ca8480e724a01e9e726388
Juho Laihanen <juho.laihanen@gmail.com>
2017-07-28 12:56:08 +0300

    Sprint1
    Gravity, simple movement and test assets

---
+++ From Dark/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    // Use this for initialization
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+        // Horizontal movement
+        move.x = Input.GetAxis ("Horizontal");
+        // Player can jump if on ground
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+		// Let's the player cancel their jump mid air
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+}
